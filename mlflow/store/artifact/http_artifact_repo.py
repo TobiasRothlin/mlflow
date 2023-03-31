0228c974c386a25bb0e39323e75809cfcb8baab8
tobias.rothlin <tobias.rothlin@ost.ch>
2023-03-31 14:03:50 +0200

    Checking if the downloader made progress, if not the downloader will raise an error

--- mlflow/store/artifact/http_artifact_repo.py
+++ mlflow/store/artifact/http_artifact_repo.py
@@ -99,7 +99,8 @@ class HttpArtifactRepository(ArtifactRepository):
         file_size_header = resp.headers.get('content-length')
         if file_size_header is not None:
             expected_file_size = int(file_size_header)
-            if (not file_size_downloaded == expected_file_size) and (file_size_downloaded > start_position):
+            if (not file_size_downloaded == expected_file_size) and (
+                    file_size_downloaded > start_position if start_position else 0):
 
                 self._partial_download(remote_file_path=remote_file_path, local_path=local_path,
                                        start_position=file_size_downloaded)
